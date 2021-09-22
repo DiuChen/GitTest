9fb699b67a0cc2899b7be372f61ca420e5dddb71
LiuChen <2248291876@qq.com>
2021-09-22 12:01:11 +0800

    重新初始化

--- app/src/main/java/com/liuchen/gittest/MainActivity.java
+++ app/src/main/java/com/liuchen/gittest/MainActivity.java
@@ -9,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //第一次编辑
-        //第二次编辑
-        //第三次编辑
-        //在分支1中第一次编辑
-        //在分支1中第二次编辑
-        //在分支1中第三次编辑
-        //在分支1中第四次编辑
-        //第四次编辑
-        //第五次编辑
-        //第六次编辑
-        //第七次编辑
     }
 }
